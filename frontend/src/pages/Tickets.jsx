e407a9b0f87256194091a07c6584a9e53a4368ee
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 06:33:52 +0000

    Tickets page

---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,17 @@
+function Tickets() {
+  return (
+    <>
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Tickets;
