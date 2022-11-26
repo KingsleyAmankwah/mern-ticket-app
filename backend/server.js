15ece610b20662abdb8fb50d74009946eea36a28
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 03:34:07 +0000

    Database connction

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
+const colors = require("colors");
+const connectDB = require("./config/db");
 const Port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
