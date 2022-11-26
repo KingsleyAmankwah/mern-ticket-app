6863a5cd378feeb0845cd5cfb598f29bd3956f45
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 22:16:16 +0000

    reset reducer removed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../features/auth/authSlice";
+import { logout } from "../features/auth/authSlice";
 
 function Header() {
   const navigate = useNavigate();
@@ -11,7 +11,6 @@ function Header() {
 
   const onLogout = () => {
     dispatch(logout());
-    dispatch(reset());
     navigate("/");
   };
   return (
