d10f49a1a0bf1d5c56834c7c583224d41067bb45
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 05:11:28 +0000

    errorhandler Added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middlewares/errorMiddleware");
 const Port = process.env.PORT || 8000;
 
 connectDB();
@@ -9,4 +10,7 @@ connectDB();
 const app = express();
 
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
 app.listen(Port, () => console.log(`Server started on Port ${Port}`));
