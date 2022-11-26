e8e4822312f2eb29d5389f0a934f6f185cfe2d9f
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 22:56:25 +0000

    Login Page Setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,54 @@
+import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login } from "../features/auth/authSlice";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData))
+      .unwrap()
+      .then((user) => {
+        // NOTE: by unwrapping the AsyncThunkAction we can navigate the user after
+        // getting a good response from our API or catch the AsyncThunkAction
+        // rejection to show an error message
+        toast.success(`Logged in as ${user.name}`);
+        navigate("/");
+      })
+      .catch(toast.error);
+  };
+
+  if (isLoading) {
+    return <>...Loading</>;
+  }
+
   return (
     <>
       <section className="heading">
@@ -11,17 +59,16 @@ function Login() {
       </section>
 
       <section className="form">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="email"
               className="form-control"
               id="email"
               name="email"
-              value=""
-              onChange=""
+              value={email}
+              onChange={onChange}
               placeholder="Enter your email"
-              required
             />
           </div>
           <div className="form-group">
@@ -30,10 +77,9 @@ function Login() {
               className="form-control"
               id="password"
               name="password"
-              value=""
-              onChange=""
+              value={password}
+              onChange={onChange}
               placeholder="Enter password"
-              required
             />
           </div>
           <div className="form-group">
