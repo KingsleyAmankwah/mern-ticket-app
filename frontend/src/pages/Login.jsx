78288c25bf2690a8ea5e6955aa599fce21e0efca
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 04:11:37 +0000

    Login Page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import { FaSignInAlt } from "react-icons/fa";
+
+function Login() {
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value=""
+              onChange=""
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value=""
+              onChange=""
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Login;
