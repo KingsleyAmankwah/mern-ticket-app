0e809a0a3970f05de79140eeaf6a14df0a117b71
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-26 06:32:38 +0000

    New Ticket Page

---
+++ frontend/src/pages/New Ticket.jsx
@@ -0,0 +1,47 @@
+function NewTicket() {
+  return (
+    <>
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value="" disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Customer Email</label>
+          <input type="text" className="form-control" value="" disabled />
+        </div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="product">Product</label>
+            <select name="product" id="product" value="">
+              <option value="iPhone">iPhone</option>
+              <option value="Macbook Pro">Macbook Pro</option>
+              <option value="iMac">iMac</option>
+              <option value="iPad">iPad</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of the issue</label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              placeholder="Description"
+              value=""
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default NewTicket;
