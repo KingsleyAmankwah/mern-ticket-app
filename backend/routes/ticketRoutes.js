b7da70cab1c70d86d590c2043c5f586244aa78c9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-27 00:42:51 +0000

    Ticket Routes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,5 +1,20 @@
 const express = require("express");
-
+const {
+  createTicket,
+  getTicket,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+  getTickets,
+} = require("../controllers/ticketController");
 const router = express.Router();
+
+const { protect } = require("../middlewares/authMiddleware");
+
+router.post("/", protect, createTicket);
+router.get("/", protect, getTickets);
+router.get("/:id", protect, getTicket);
+router.put("/:id", protect, updateTicket);
+router.delete("/:id", protect, deleteTicket);
 
 module.exports = router;
