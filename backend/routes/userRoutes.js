9c6adbaaf7ff7e0d29188d8c83f9ecd6bf3e7e70
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 18:09:45 +0000

    user Routes setup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,12 @@ const {
   getMe,
 } = require("../controllers/userControllers");
 
+const { protect } = require("../middlewares/authMiddleware");
+
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;
