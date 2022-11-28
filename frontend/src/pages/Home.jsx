f70381b460e1dd4ef759074954593b32f872f1e2
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-28 03:46:32 +0000

    Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <section className="heading">
-        <h1>What do you need help with?</h1>
+        <h1>Welcome to Ticket Desk</h1>
         <p>Please choose from an option below</p>
       </section>
 
