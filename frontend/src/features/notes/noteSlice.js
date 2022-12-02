616edc5aa082db2e25f51d8ba0be87da1608d996
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-02 04:34:18 +0000

    note Slice

---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { extractErrorMessage } from "../../utils/utils";
+import noteService from "./noteService";
+
+const initialState = {
+  notes: null,
+};
+
+//Get notes
+export const getNotes = createAsyncThunk(
+  "note/getAll",
+  async (ticketId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.getNotes(ticketId, token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
+//Add Note
+export const addNote = createAsyncThunk(
+  "note/create",
+  async ({ noteText, ticketId }, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.addNote(noteText, ticketId, token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
+export const noteSlice = createSlice({
+  name: "note",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNotes.pending, (state) => {
+        state.notes = null;
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.notes = action.payload;
+      })
+      .addCase(addNote.fulfilled, (state, action) => {
+        state.notes.push(action.payload);
+      });
+  },
+});
+
+export default noteSlice.reducer;
