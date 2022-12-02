3eff3fd579f79be79d9ca8731586e829de34334d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-02 03:54:43 +0000

    note Route added

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+app.use("/api/notes", require("./routes/noteRoutes"));
 
 app.use(errorHandler);
 
