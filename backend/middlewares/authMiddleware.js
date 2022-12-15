dcb36fab65bbdc3820202bfe82624b42adaf6398
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-15 19:39:25 +0000

    auth middleware modified

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,35 +1,29 @@
 const jwt = require("jsonwebtoken");
+
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      //Get toekn from header
-      token = req.headers.authorization.split(" ")[1];
-
-      //Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      //Get user from token
-      req.user = await User.findById(decoded.id).select("-password");
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error("Not authorized");
-    }
-  }
+  let token = req.cookies.token;
 
   if (!token) {
     res.status(401);
     throw new Error("Not authorzied, no token");
   }
+
+  //verify token
+  const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+  // Get user id from token
+  const user = await User.findById(verifiedToken.id).select("-password");
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  req.user = user;
+  next();
 });
 
 module.exports = { protect };
