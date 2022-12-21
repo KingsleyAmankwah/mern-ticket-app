7ea3459c643bb33ded64c8e9103e42d285ad3b52
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-21 21:20:29 +0000

    ticket

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -4,13 +4,13 @@ const API_URL = "/api/tickets/";
 
 //Create new ticket
 const createTicket = async (ticketData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+  // const config = {
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // };
 
-  const response = await axios.post(API_URL, ticketData, config);
+  const response = await axios.post(API_URL, ticketData, token);
   return response.data;
 };
 
