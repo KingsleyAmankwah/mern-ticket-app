277fcc59891ad5b0ef512ac1bb5b3339d3bd1a46
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-21 21:19:28 +0000

    auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,10 +1,9 @@
 const jwt = require("jsonwebtoken");
-
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token = req.cookies.token;
+  const token = req.cookies.accessToken;
 
   if (!token) {
     res.status(401);
