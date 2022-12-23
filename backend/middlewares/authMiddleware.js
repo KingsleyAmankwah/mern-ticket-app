0834fa6a5571e60ddc819433eb8d04c2d557f050
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-23 02:46:54 +0000

    auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,35 +3,37 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-  const token = req.cookies.accessToken;
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error("Not authirised");
+      }
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
 
   if (!token) {
     res.status(401);
-    throw new Error("Not authorzied, no token");
-  }
-
-  //verify token
-  const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-  // Get user id from token
-  const user = await User.findById(verifiedToken.id).select("-password");
-
-  if (!user) {
-    res.status(404);
-    throw new Error("User not found");
-  }
-
-  req.user = user;
-  next();
-});
-
-const adminOnly = asyncHandler(async (req, res, next) => {
-  if (req.user && req.user.isAdmin === true) {
-    next();
-  } else {
-    res.status(401);
-    throw new Error("Not authorized as an admin");
+    throw new Error("Not authorized");
   }
 });
 
-module.exports = { protect, adminOnly };
+module.exports = { protect };
