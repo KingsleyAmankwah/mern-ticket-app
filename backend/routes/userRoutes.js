e9f5f3a5b06e66f382a4a59402e1a0b180f881ce
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-23 02:47:19 +0000

    user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
-  logoutUser,
   getMe,
 } = require("../controllers/userControllers");
 
@@ -12,7 +11,6 @@ const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/logout", logoutUser);
 router.get("/me", protect, getMe);
 
 module.exports = router;
