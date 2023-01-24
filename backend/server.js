7e3853fe2de5f0c1f7e3282246312b2af63d2f9f
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-24 01:34:23 +0000

    Serve frontend in production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 // const cookieParser = require("cookie-parser");
 // const cors = require("cors");
 const Port = process.env.PORT || 8000;
@@ -17,6 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 app.use("/api/notes", require("./routes/noteRoutes"));
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" });
+  });
+}
 
 app.use(errorHandler);
 
